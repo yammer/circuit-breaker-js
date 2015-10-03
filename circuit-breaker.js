289aa1cb2b3f37572f6e06f488b778d41ad343ea
rupamroy <rupamroy@users.noreply.github.com>
2015-10-02 21:34:21 -0400

    Update notification from half open to Open state
    When we move from half-open to open state - the notification is missing. I would like to include that.

--- circuit-breaker.js
+++ circuit-breaker.js
@@ -150,6 +150,7 @@
 
       if (lastCommandFailed) {
         this._state = CircuitBreaker.OPEN;
+        this.onCircuitOpen(metrics);
       }
       else {
         this._state = CircuitBreaker.CLOSED;
